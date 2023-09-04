18e50f4d0c1f3a4b23a1d6710ddf5be2a300ca8f
MyMeShell <bussacmichel@yahoo.fr>
2023-09-04 18:21:48 +0200

    création liste de pokémons et affichage dans des conteneurs dans avec le style MATERIALIZE

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
+import Pokemon from "./models/pokemon";
+import { POKEMONS } from "./models/mock-pokemon";
 
 const App: FunctionComponent = () => {
-    const name: String = 'React';
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]); //const name: String = 'React';
+
+    useEffect(() => {
+        setPokemons(POKEMONS);
+    }, []);
 
     return (
-        <h1>Hello, {name} !</h1>
+        <div>
+            <h1 className="center">Pokédex</h1>
+            <div className="container">
+                <div className="row">
+                {pokemons.map(({id, name, picture, created}) => (
+                    <div className="col s6 m4" key={id}>
+                    <div className="card horizontal">
+                        <div className="card-image">
+                        <img src={picture} alt={name}/>
+                        </div>
+                        <div className="card-stacked">
+                        <div className="card-content">
+                            <p>{name}</p>
+                            <p><small>{created.toDateString()}</small></p>
+                        </div>
+                        </div> 
+                    </div>
+                    </div>
+                ))}
+
+                </div>
+            </div>   
+        </div>
     )
 }
 
