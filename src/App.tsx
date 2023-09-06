244be0326b8e0b836db98547f8e1352ae38b0d73
MyMeShell <bussacmichel@yahoo.fr>
2023-09-06 07:08:40 +0200

    Le tuto est terminé mais j'ajoute une barre de recherche qui filtre en fonction du nom du pokémon et j'ajoute un fond 'écran

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,53 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
 import Pokemon from "./models/pokemon";
 import { POKEMONS } from "./models/mock-pokemon";
+import "./App.css"; // Importez le fichier CSS
 
 const App: FunctionComponent = () => {
-    const [pokemons, setPokemons] = useState<Pokemon[]>([]); //const name: String = 'React';
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    const [searchText, setSearchText] = useState<string>("");
 
     useEffect(() => {
         setPokemons(POKEMONS);
     }, []);
 
+    const filteredPokemons = pokemons.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
     return (
-        <div>
+        <div className="light-body">
             <h1 className="center">Pokédex</h1>
+            <div className="search-bar-container">
+                <input
+                    type="text"
+                    placeholder="Rechercher un Pokémon..."
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                    className="search-bar-input" // Appliquer une classe CSS pour le style
+                />
+            </div>
             <div className="container">
                 <div className="row">
-                {pokemons.map(({id, name, picture, created}) => (
-                    <div className="col s6 m4" key={id}>
-                    <div className="card horizontal">
-                        <div className="card-image">
-                        <img src={picture} alt={name}/>
+                    {filteredPokemons.map(({ id, name, picture, created }) => (
+                        <div className="col s6 m4" key={id}>
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img src={picture} alt={name} />
+                                </div>
+                                <div className="card-stacked">
+                                    <div className="card-content">
+                                        <p>{name}</p>
+                                        <p><small>{created.toDateString()}</small></p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="card-stacked">
-                        <div className="card-content">
-                            <p>{name}</p>
-                            <p><small>{created.toDateString()}</small></p>
-                        </div>
-                        </div> 
-                    </div>
-                    </div>
-                ))}
-
+                    ))}
                 </div>
-            </div>   
+            </div>
         </div>
-    )
+    );
 }
 
 export default App;
