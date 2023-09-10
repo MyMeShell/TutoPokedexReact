8a6904976d76c27d6baf18d84f54aad854ea9e1f
MyMeShell <bussacmichel@yahoo.fr>
2023-09-10 14:04:53 +0200

    Modification du bouton pokéball et du titre en fonction du thème

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,31 @@ import "./App.css"; // Importez le fichier CSS
 const App: FunctionComponent = () => {
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
     const [searchText, setSearchText] = useState<string>("");
+    const [theme, setTheme] = useState<"light" | "dark">("light"); // État pour suivre le thème
 
     useEffect(() => {
         setPokemons(POKEMONS);
     }, []);
+
+    const toggleTheme = () => {
+        // Bascule entre les thèmes lorsque le bouton est cliqué
+        setTheme(theme === "light" ? "dark" : "light");
+    };
 
     const filteredPokemons = pokemons.filter(pokemon =>
         pokemon.name.toLowerCase().includes(searchText.toLowerCase())
     );
 
     return (
-        <div className="light-body">
-            <h1 className="center">POKÉDEX   <button className="poke-button"></button> </h1>
+        <div className={theme === "light" ? "light-body" : "dark-body"}>
+            <h1 className={theme === "light" ? "center title-light" : "center title-dark"}>POKÉDEX <button className={theme === "light" ? "poke-button-light" : "poke-button-dark"} onClick={toggleTheme}></button></h1>
             <div className="search-bar-container">
                 <input
                     type="text"
                     placeholder="Rechercher un Pokémon..."
                     value={searchText}
                     onChange={(e) => setSearchText(e.target.value)}
-                    className="search-bar-input" // Appliquer une classe CSS pour le style
+                    className="search-bar-input"
                 />
             </div>
             <div className="container">
